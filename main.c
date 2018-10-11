5a93984f7d23510bdf82e84c55b2611aaa9fa6fd
김주홎 <gyong0812@naver.com>
2018-10-11 16:35:11 +0900

    up-down game

--- main.c
+++ main.c
@@ -5,26 +5,38 @@
 
 int main(int argc, char *argv[]) {
 	
-	int num=0; //초기화 하기! 
-	char str[100]; //배열의 크기를 100으로 설정한거임. 
-	char c;
+	int x;
+	int answer = 59;
+	int num=0;
 	
-	
-	printf("input a string:");
-	//scanf("%s);
-	
-	while((c=getchar())!='\n') //한글자씩 가져와서 c에 저장을 해준다.
+	do
 	{
-		if('0'<=c && '9'>=c)
+		printf("Guess a number:");
+		scanf("%d",&x);
+		num++;
 		
+			
+		if(x<answer)
 		{
-			num++;
+			printf("low!\n");
 		}
-	}	
+		
+		else if(x>answer)
+		{
+			printf("high!\n");
+		}
+		
+		else
+		{
+			printf("Congratulation");
+		}
+		
+		
+	}while(x !=answer);
 	
-	 printf("the number of digits are %d", num);
+	printf("trial : %i\n",num);
 	
-
+	
 	return 0;	
 	
 }
